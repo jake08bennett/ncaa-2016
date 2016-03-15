8ad2334433ad72fd59d7ac49c7618d4a98e64c89
jake08bennett <jake08bennett@hotmail.com>
2016-03-15 10:13:38 -0600

    progress happening slowly

---
+++ src/ReadFile.java
@@ -0,0 +1,39 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ReadFile {
+	public static void ReadData(String[] args) {
+		
+		ArrayList<String> data = new ArrayList<String>();
+		ArrayList<Team> teams = new ArrayList<Team>();
+		
+		try {
+			Scanner input = new Scanner(System.in);
+			
+			File file = new File("ncaa_data_2016.csv");  
+			
+			Scanner scanner = input = new Scanner(file);
+			
+			while(input.hasNextLine()) {
+				String line = input.nextLine();
+				data.add(line);
+			 }
+			
+			for (int i = 1; i < 65; i++ ) {
+				String list = (data.get(i));
+				String[] help = list.split(",");
+				for (int j = 0; j < 3; j++) {
+					Team team = new Team(help[0],Double.parseDouble(help[1]), help[2], Integer.parseInt(help[3]));
+					teams.add(team);
+				  }
+			   }
+			 input.close();
+			
+			}  catch (Exception ex) {
+			ex.printStackTrace();
+			  }
+	
+		}
+	
+}
---
+++ src/Team.java
@@ -0,0 +1,16 @@
+public class Team {
+	private String name;
+	private int seed;
+	private double expected;
+	private String region;
+	public Team(String name, double expected, String region, int seed) {
+		this.seed = seed;
+		this.region = region;
+		this.expected = expected;
+		this.name = name;
+	}
+	public String toString() {
+		return (name + " " + seed + " " + region + " " +  expected);
+	}
+
+}
